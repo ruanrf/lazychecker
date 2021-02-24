214221ef0b68cc77db041bd21d8822a0d691d6a2
ruanrf <ruan_rf@hotmail.com>
2021-02-24 17:29:36 -0300

    development

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,10 @@
+import '../styles/global.css'
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
 
 export default MyApp
+
+// no arquivo "_app", de modo geral,
+// ficam componentes presentes em todas páginas da aplicação
