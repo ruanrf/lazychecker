3496af1e847f6cd6b924778f82604b3d41a869c5
ruanrf <ruan_rf@hotmail.com>
2021-02-24 17:29:28 -0300

    add components to index page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,28 @@
+import { ExperienceBar } from '../components/ExperienceBar';
+import { Profile } from '../components/Profile';
+import { CompletedChallenges } from '../components/CompletedChallenges';
+import { Countdown } from '../components/Countdown';
 
+import Head from 'next/head';
+
+import styles from '../styles/pages/Home.module.css';
 
 export default function Home() {
   return (
-    <h1>teste</h1>
+  <div className={styles.container}>
+    <Head><title>Início | MindCycle</title></Head>
+    <ExperienceBar />
+
+    <section>
+      <div>
+        <Profile />
+        <CompletedChallenges />
+        <Countdown />
+      </div>
+      <div>
+
+      </div>
+    </section>
+  </div>
   )
 }
